c1b1128c25b4764673daf6f6a80f5542520b42c9
Vahdettin Karataş <38797434+vahdetkaratas@users.noreply.github.com>
2024-07-18 22:48:31 +0200

    update-user api route

--- server/index.js
+++ server/index.js
@@ -3,20 +3,23 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log('MongoDb is connected');
   })
   .catch((err) => {
     console.log(err);
   });
 
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
