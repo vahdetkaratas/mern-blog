db0b41fd0c0e81ef5abaa0960234157d47e33a22
vahdet <vahdet.karatas@gmail.com>
2024-08-05 12:43:39 +0200

    added like-edit-delete functionalities to the comment section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -67,8 +67,7 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full'>
-      </div>
+    
       <CommentSection postId={post._id} />
     </main>
   );
